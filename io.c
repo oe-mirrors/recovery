78c2188238bd918345281d942017a33f39d9a5d7
Andreas Oberritter <obi@opendreambox.org>
2019-03-13 21:32:35 +0100

    global: change web link to dreambox.de

--- io.c
+++ io.c
@@ -2,7 +2,7 @@
  * io.c
  *
  * Copyright (C) 2016 Dream Property GmbH, Germany
- *                    http://www.dream-multimedia-tv.de/
+ *                    https://dreambox.de/
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
